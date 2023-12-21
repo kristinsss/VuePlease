d5eede7a3415a0f9b4d06eea14afaeca98cfc8af
kristinsss <151121672+kristinsss@users.noreply.github.com>
2023-12-21 13:33:04 +0000

    Update tasks.json and vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -30,8 +30,5 @@ module.exports = {
       title: "Delivery",
       chunks: ["chunk-vendors", "chunk-common", "delivery"],
     },
-    devServer: {
-      disableHostCheck: true,
-    },
   },
 };
