99d1edefe7ac001477804e28366950962d1bd8ef
kristinsss <151121672+kristinsss@users.noreply.github.com>
2023-12-21 13:30:43 +0000

    Update 3 files

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,37 @@
 module.exports = {
   pages: {
-    'index': {
-      entry: './src/pages/Home/main.js',
-      template: 'public/index.html',
-      title: 'Home',
-      chunks: [ 'chunk-vendors', 'chunk-common', 'index']
+    index: {
+      entry: "./src/pages/Home/main.js",
+      template: "public/index.html",
+      title: "Home",
+      chunks: ["chunk-vendors", "chunk-common", "index"],
     },
-    'aboutus': {
-      entry: './src/pages/About/main.js',
-      template: 'public/aboutus.html',
-      title: 'About us',
-      chunks: [ 'chunk-vendors', 'chunk-common', 'aboutus']
+    aboutus: {
+      entry: "./src/pages/About/main.js",
+      template: "public/aboutus.html",
+      title: "About us",
+      chunks: ["chunk-vendors", "chunk-common", "aboutus"],
     },
-    'cart': {
-      entry: './src/pages/Cart/main.js',
-      template: 'public/cart.html',
-      title: 'Cart',
-      chunks: [ 'chunk-vendors', 'chunk-common', 'cart']
+    cart: {
+      entry: "./src/pages/Cart/main.js",
+      template: "public/cart.html",
+      title: "Cart",
+      chunks: ["chunk-vendors", "chunk-common", "cart"],
     },
-    'catalog': {
-      entry: './src/pages/Catalog/main.js',
-      template: 'public/catalog.html',
-      title: 'Catalog',
-      chunks: [ 'chunk-vendors', 'chunk-common', 'catalog']
+    catalog: {
+      entry: "./src/pages/Catalog/main.js",
+      template: "public/catalog.html",
+      title: "Catalog",
+      chunks: ["chunk-vendors", "chunk-common", "catalog"],
     },
-    'delivery': {
-      entry: './src/pages/Delivery/main.js',
-      template: 'public/delivery.html',
-      title: 'Delivery',
-      chunks: [ 'chunk-vendors', 'chunk-common', 'delivery']
+    delivery: {
+      entry: "./src/pages/Delivery/main.js",
+      template: "public/delivery.html",
+      title: "Delivery",
+      chunks: ["chunk-vendors", "chunk-common", "delivery"],
     },
-  }
-}
+    devServer: {
+      disableHostCheck: true,
+    },
+  },
+};
